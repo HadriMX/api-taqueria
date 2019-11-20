666a79988126327a6c3ba228417a7d512bd9f90c
Adrián Reyes <beltran17_98@hotmail.com>
2019-11-19 17:56:24 -0700

    Added ProveedoresController

---
+++ ApiTaqueria/Controllers/ProveedoresController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiTaqueria.Persistence;
+using ApiTaqueria.Persistence.Entities;
+
+namespace ApiTaqueria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedoresController : ControllerBase
+    {
+        private readonly TaqueriaContext _context;
+
+        public ProveedoresController(TaqueriaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Proveedores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Proveedores>>> GetProveedores()
+        {
+            return await _context.Proveedores.ToListAsync();
+        }
+
+        // GET: api/Proveedores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Proveedores>> GetProveedores(int id)
+        {
+            var proveedores = await _context.Proveedores.FindAsync(id);
+
+            if (proveedores == null)
+            {
+                return NotFound();
+            }
+
+            return proveedores;
+        }
+
+        // PUT: api/Proveedores/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProveedores(int id, Proveedores proveedores)
+        {
+            if (id != proveedores.IdProveedor)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(proveedores).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProveedoresExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Proveedores
+        [HttpPost]
+        public async Task<ActionResult<Proveedores>> PostProveedores(Proveedores proveedores)
+        {
+            _context.Proveedores.Add(proveedores);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProveedores", new { id = proveedores.IdProveedor }, proveedores);
+        }
+
+        // DELETE: api/Proveedores/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Proveedores>> DeleteProveedores(int id)
+        {
+            var proveedores = await _context.Proveedores.FindAsync(id);
+            if (proveedores == null)
+            {
+                return NotFound();
+            }
+
+            _context.Proveedores.Remove(proveedores);
+            await _context.SaveChangesAsync();
+
+            return proveedores;
+        }
+
+        private bool ProveedoresExists(int id)
+        {
+            return _context.Proveedores.Any(e => e.IdProveedor == id);
+        }
+    }
+}
